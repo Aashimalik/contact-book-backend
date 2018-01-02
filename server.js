eb6cd43e380e3a44386887499bdd11031dfc9070
ayesha_malik <ayesha_malik@seologistics.com>
2018-01-02 16:51:17 +0530

    get and post api

--- server.js
+++ server.js
@@ -5,40 +5,63 @@ var http=require('http').Server(app);
 var mongoose=require('mongoose');
 var bodyParser=require('body-parser');
 var Contact = require('./model/contact');
-async      = require('async'),
+async      = require('async');
+var cors = require('cors');
 
 
 mongoose.connect('mongodb://localhost:27017/contact');
 app.use(bodyParser.urlencoded({extended:true}));
-
+ var whitelist = ['http://localhost', 'http://localhost:3001','http://localhost:3000','http://localhost:8000','localhost:8000/contact']
 
 app.use(bodyParser.json());
+
+var corsOptionsDelegate = function (req, callback) {
+  var corsOptions;
+  if (whitelist.indexOf(req.header('Origin')) !== -1) {
+    corsOptions = { origin: function (origin, callback) {
+        console.log(origin)
+        if (whitelist.indexOf(origin) !== -1) {
+          callback(null, true)
+        } else {
+          callback(new Error('Not allowed by CORS'))
+        }
+      } } // reflect (enable) the requested origin in the CORS response 
+  }else{
+    corsOptions = { origin: function (origin, callback) {
+        console.log(origin)
+        if (whitelist.indexOf(origin) !== -1) {
+          callback(null, true)
+        } else {
+          callback(new Error('Not allowed by CORS'))
+        }
+      } } // disable CORS for this request 
+  }
+  callback(null, corsOptions) // callback expects two parameters: error and options 
+}
+// app.use(cors(corsOptionsDelegate))
+ app.use(cors())
+// app.use(cors({
+//     origin: function (origin, callback) {
+//         console.log(origin)
+//         if (whitelist.indexOf(origin) !== -1) {
+//           callback(null, true)
+//         } else {
+//           callback(new Error('Not allowed by CORS'))
+//         }
+//       },
+// }))
 
 app.get('/api/contacts',function (req, res) {
     let obj=req.query;
     // var limit=page.limit ?parseInt(page.limit):5
-   let limit=2
+   let limit=5
   let page=(obj.page)?parseInt(obj.page):1
   let _skip=(page-1)*limit;
-  console.log(page)
-  console.log(_skip)
-
-    console.log("inside get api");
+  
     async.parallel({
         Count:(callback)=>{
-            Contact.aggregate(
-                [
-                   { $group: { _id: null, count: { $sum: 1 } } },
-                  
-                //    { 
-                //     $skip :_skip
-                //    },
-                //    { 
-                //     $limit : limit 
-                //    }
-                ],callback)
-        },
-        Records:(callback)=>{
+            Contact.count(callback)},
+        contacts:(callback)=>{
             Contact.aggregate(
                 [
                     {
@@ -62,10 +85,16 @@ app.get('/api/contacts',function (req, res) {
 
     },(err,result)=>{
         if(err){
-            return res.json({message:"error"})
+            return res.status(500).json({message:err})
         }
         else{
-            return res.json({data:result})
+            let countResult = (result.contacts) ? result.contacts.length : 0,
+            pageCount = countResult !== 0 ? parseInt(Math.ceil(result.Count / limit) ):0;
+            return res.status(200).json(
+                {
+                    contacts:result.contacts,
+                    pageCount:pageCount
+                })
         }
     })
    
@@ -99,13 +128,6 @@ app.post('/contact',  function (req, res) {
     })
 });
 
-// app.use((req, res) => {
-//     // console.log(req.headers);
-//     res.statusCode = 200;
-//     res.setHeader('Content-Type', 'text/html');
-//     res.end("connected to server");
-    
-//     });
 
 
 http.listen(PORT, function(err){
